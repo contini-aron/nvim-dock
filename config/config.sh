3eed498a00ea195a848190388aacde3fdcbca10a
contini-aron <100793455+contini-aron@users.noreply.github.com>
2022-09-11 23:17:41 +0200

    deleted misleading line

--- config/config.sh
+++ config/config.sh
@@ -1,3 +1,2 @@
 # install packer.nvim
-# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone --depth 1 https://github.com/contini-aron/nvim-conf.git ~/.config/nvim
